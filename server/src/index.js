6a5bcedeba38a7b97caa2c4d5041088ccfe53925
Jash2511 <jashdpatel2511@gmail.com>
2025-10-24 21:17:20 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,8 @@
-import dotenv from 'dotenv';
+
 import app from './app.js';
 import { connectDB } from './utils/db.js';
 
-dotenv.config();
+
 
 const PORT = process.env.PORT || 4000;
 
@@ -14,3 +14,4 @@ connectDB()
     console.error('Failed to connect DB', e);
     process.exit(1);
   });
+
